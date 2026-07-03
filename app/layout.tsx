831b061b390a4543693cb7bdd0a5ae9fea814476
alexb06 <alexanderblasbalg@gmail.com>
2026-07-03 16:22:28 -0400

    added auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import { NeonAuthUIProvider, UserButton } from "@neondatabase/auth-ui";
+import { authClient } from "@/lib/auth/client";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,8 +29,10 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col bg-[var(--background)] text-[var(--foreground)]">
+        <NeonAuthUIProvider authClient={authClient}>
         <header className="border-b border-[var(--border)] bg-[var(--surface)]/90 backdrop-blur">
           <div className="mx-auto flex h-16 w-full max-w-6xl items-center justify-between px-4">
             <Link href={"/"} className="text-sm font-semibold tracking-wide">Event Planner</Link>
@@ -39,11 +43,15 @@ export default function RootLayout({
               >
                 Dashboard
               </Link>
+              <UserButton size="icon"></UserButton>
             </nav>
           </div>
         </header>
-
-        <main className="mx-auto flex w-full max-w-6xl flex-1 flex-col px-4 py-8">{children}</main>
+        
+        <main className="mx-auto flex w-full max-w-6xl flex-1 flex-col px-4 py-8">
+          {children}
+        </main>
+        </NeonAuthUIProvider>
       </body>
     </html>
   );
---
+++ proxy.ts
@@ -0,0 +1,12 @@
+// proxy.ts
+import { auth } from '@/lib/auth/server';
+
+export default auth.middleware({
+  loginUrl: '/auth/sign-in',
+});
+
+export const config = {
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico).*)",
+  ],
+};
